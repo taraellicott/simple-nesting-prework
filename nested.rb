8e4c626da93e95116107b4515f82f80f896ad5c4
Tara Ellicott <taraellicott@gmail.com>
2018-04-14 16:03:34 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -123,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]
+programmer_hash[:dennis_ritchie][:languages].second = "Assembly"
+programmer_hash
 end
